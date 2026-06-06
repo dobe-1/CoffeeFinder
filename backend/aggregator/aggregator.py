b4989cf7bdc5bda970080f65dc8726e7b3caade6
Jonas-912 <72095219+Jonas-912@users.noreply.github.com>
2026-06-06 12:50:22 +0200

    small change in regex added todos for tasks

--- backend/aggregator/aggregator.py
+++ backend/aggregator/aggregator.py
@@ -4,7 +4,26 @@ from pathlib import Path
 
 from backend.models import CoffeeShop
 from backend.scraper.overpassAPI import get_coffee_shops_in_city
-from backend.scraper.web_scraper import extract_menu_url_from_coffee_shop
+from backend.scraper.web_scraper import (
+    extract_menu_url_from_coffee_shop,
+    get_menu_urls_from_website,
+)
+
+# TODO function for comparing execution with test set
+#   - load test set from file (coffee shops from city with correct menu links)
+#   - execute get_coffe_shops
+#   - check for each coffee shop: is correct menu from test in return list from get_coffe_shops
+#   - return ratio
+
+# TODO function to create test set
+#   - specify city
+#   - execute get_coffe_shops
+#   - store to file
+#
+
+
+def get_menu_for_url(url: str) -> list:
+    return get_menu_urls_from_website(url)
 
 
 def get_coffee_shops(city: str) -> list[CoffeeShop]:
@@ -19,14 +38,14 @@ def get_coffee_shops(city: str) -> list[CoffeeShop]:
             coffee_shops = [CoffeeShop.model_validate(coffee_shop) for coffee_shop in json.load(f)]
 
     # Check if the coffee shops have price information
+    # TODO fix logic to correctly extract menu if not cached
     for coffee_shop in coffee_shops:
         if (
             coffee_shop.website.url
-            and coffee_shop.website.accessible != False
-            and not coffee_shop.menu.extracted_at
-            or (
-                coffee_shop.menu.extracted_at
-                and coffee_shop.menu.extracted_at <= datetime.now(tz=UTC) - timedelta(days=30)
+            and coffee_shop.website.accessible
+            and (
+                not coffee_shop.menu.extracted_at
+                or coffee_shop.menu.extracted_at <= datetime.now(tz=UTC) - timedelta(days=30)
             )
         ):
             extract_menu_url_from_coffee_shop(coffee_shop)
